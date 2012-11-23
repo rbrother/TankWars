e7b0a827e1fdde7e79fc4670abb8cefeb6775659
Robert Brotherus <robert@iki.fi>
2012-11-23 22:21:29 +0200

    Tank moving and shooting working

--- TankWars/Game1.cs
+++ TankWars/Game1.cs
@@ -10,15 +10,24 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
 namespace TankWars {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+
     public class Game1 : Microsoft.Xna.Framework.Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D _tankTexture;
+        Texture2D _bulletTexture;
+        float _secondsElapsed;
+        float _tankRotation = 0.0f;
+        Vector2 _tankPosition = new Vector2( 200.0f, 200.0f );
+        IEnumerable<Tuple<Vector2, Vector2>> _bullets = new List<Tuple<Vector2, Vector2>>();
+        float _rotationSpeed = 2.0f;
+        float _bulletSpeed = 2.0f;
+        MouseState _previousMouseState = new MouseState();
 
         public Game1( ) {
             graphics = new GraphicsDeviceManager( this );
+            graphics.IsFullScreen = false;
+            graphics.PreferMultiSampling = true;
             Content.RootDirectory = "Content";
         }
 
@@ -42,6 +51,8 @@ namespace TankWars {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch( GraphicsDevice );
 
+            _tankTexture = this.Content.Load<Texture2D>( "tank" );
+            _bulletTexture = this.Content.Load<Texture2D>( "bullet" );
             // TODO: use this.Content to load your game content here
         }
 
@@ -62,11 +73,43 @@ namespace TankWars {
             // Allows the game to exit
             if (GamePad.GetState( PlayerIndex.One ).Buttons.Back == ButtonState.Pressed)
                 this.Exit( );
+            _secondsElapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var keyboard = Keyboard.GetState( );
+            if (keyboard.IsKeyDown( Keys.A )) {
+                _tankRotation -= _rotationSpeed * _secondsElapsed; 
+            } else if (keyboard.IsKeyDown( Keys.D )) {
+                _tankRotation += _rotationSpeed * _secondsElapsed; 
+            }
+            if (keyboard.IsKeyDown( Keys.W ) || keyboard.IsKeyDown( Keys.S )) {
+                var move = Vector2.Transform( -Vector2.UnitY, TankRotationMatrix ) * _secondsElapsed * 100.0f;
+                move = move * ( keyboard.IsKeyDown( Keys.W ) ? 1.0f : -1.0f );
+                _tankPosition += move;                
+            }
+            var mouseState = Mouse.GetState( );
+            var leftClick = mouseState.LeftButton == ButtonState.Pressed && 
+                _previousMouseState.LeftButton == ButtonState.Released;
+            if (leftClick) {
+                var velocity = Vector2.Transform( -Vector2.UnitY, TankRotationMatrix );
+                var bullet = Tuple.Create( _tankPosition + velocity * 55.0f, velocity );
+                _bullets = _bullets.Concat( new Tuple<Vector2, Vector2>[] { bullet } );
+            }
 
-            // TODO: Add your update logic here
-
+            _bullets = _bullets
+                .Select( bullet => MoveBullet(bullet) )
+                .Where( bullet => Vector2.Distance( _tankPosition, bullet.Item1 ) < 10004 );
+            
+            _previousMouseState = mouseState;
             base.Update( gameTime );
         }
+
+        private Tuple<Vector2, Vector2> MoveBullet( Tuple<Vector2, Vector2> bullet ) {
+            return new Tuple<Vector2, Vector2>(
+                bullet.Item1 + bullet.Item2 * _secondsElapsed * 300.0f * _bulletSpeed,
+                bullet.Item2
+                );
+        }
+
+        private Matrix TankRotationMatrix { get { return Matrix.CreateRotationZ( _tankRotation ); } }
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -74,9 +117,16 @@ namespace TankWars {
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw( GameTime gameTime ) {
             GraphicsDevice.Clear( Color.CornflowerBlue );
+            spriteBatch.Begin( SpriteSortMode.BackToFront, BlendState.AlphaBlend, SamplerState.AnisotropicClamp,
+                DepthStencilState.None, RasterizerState.CullCounterClockwise );
 
-            // TODO: Add your drawing code here
-
+            spriteBatch.Draw( _tankTexture, _tankPosition, null, Color.White, _tankRotation, 
+                new Vector2(184.0f,184.0f), 0.3f, SpriteEffects.None, 0.0f);
+            foreach( Tuple<Vector2,Vector2> bullet in _bullets) {
+                spriteBatch.Draw( _bulletTexture, bullet.Item1, null, Color.White, 0.0f,
+                    new Vector2( 16.0f, 16.0f ), 0.3f, SpriteEffects.None, 0.0f );
+            }
+            spriteBatch.End( );
             base.Draw( gameTime );
         }
     }
